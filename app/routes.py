7dd8daa11697923ac76839d152d0819ac2eb6a3a
Valerie Liao <valerieliao@Valeries-MacBook-Air.local>
2022-08-15 09:11:03 +0800

    flask setup

---
+++ app/routes.py
@@ -0,0 +1,14 @@
+from app import app
+from flask import render_template
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return "Hello World!"
+
+# wait till pages to be ready to add more routes
+# @app.route('/signup', methods=['GET', 'POST'])
+# def signup():
+#    return render_template('signup.html')
+
+if __name__ == '__main__':
+   app.run(debug = True)
