7fbafaa93b1a92999dccea65df8e16ce4a6bca96
Valerie Liao <valerieliao@Valeries-MacBook-Air.local>
2022-08-15 09:11:03 +0800

    flask setup

---
+++ app/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flaskext.mysql import MySQL
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+
+app.secret_key = 'this1sKey'
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'flask'
+ 
+mysql = MySQL(app)
+migrate = Migrate(app, mysql)
+
+from app import routes
+
+if __name__ == '__main__':
+    app.run(debug=True)
