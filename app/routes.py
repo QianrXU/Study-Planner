cf5e732b6f1a305e6449a75a1e1d9c4300e2bb3c
clemmis <clementz.c@gmail.com>
2022-08-18 08:26:21 +0200

    Fixed main

--- app/routes.py
+++ app/routes.py
@@ -2,17 +2,12 @@ from app import app
 from flask import render_template
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index', methods=['GET', 'POST'])
 def index():
     #return "Hello world"
     return render_template('index.html', title='Home')
 
-""" 
-wait till pages to be ready to add more routes
-@app.route('/signup', methods=['GET', 'POST'])
-def signup():
-    return render_template('signup.html')
-""" 
+
 # Login Page
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -36,6 +31,15 @@ def createstudyplanstep1():
 def createstudyplanstep2():
     return render_template('step2-createstudyplan.html', title="Create study plan")
 
+# FAQ
+@app.route('/faq', methods=['GET', 'POST'])
+def faq():
+    return render_template('faq.html', title="FAQ")
+
+    
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    return render_template('signup.html', title='Sign up')
 
 @app.errorhandler(404)
 def page_not_found(e):
