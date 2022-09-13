0975b98bdd798d7424314cd71e651b0c6ca1b489
loveyamiyu <loveyamiyu@gmail.com>
2022-09-13 20:18:21 +0800

    added unit tests for the routes of 404 page, login page and signup page

--- test_routes_valerie.py
+++ test_routes_valerie.py
@@ -24,6 +24,24 @@ class FlaskTestCase(unittest.TestCase):
         self.assertIn(b'Do i need to sign up?', response.data)
         self.assertIn(b'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', response.data)
     
+    # Ensure a 404 page will return when a page is not found.
+    def test_page_not_found(self):
+        tester = app.test_client(self)
+        response = tester.get('/somepage', content_type='html/text')
+        self.assertEqual(response.status_code, 404)
+
+    # Test if the signup page can be rendered successfully.
+    def test_loading_signup(self):
+        tester = app.test_client(self)
+        response = tester.get('/signup', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+
+    # Test if the login page can be rendered successfully.
+    def test_loading_login(self):
+        tester = app.test_client(self)
+        response = tester.get('/login', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+    
     # Ensure when users log out, the page will be redirected to the index page.
     def test_logout(self):
         tester = app.test_client(self)
@@ -32,8 +50,8 @@ class FlaskTestCase(unittest.TestCase):
             data=dict(email='apple@mail.com', password='123456'), # test user data already exist in the database
             follow_redirects=True
         )
-        response = tester.get('/logout', follow_redirects=True)
-        self.assertTrue(b'' in response.data)
+        response = tester.get('/logout', follow_redirects=True) # the page should be redirected.
+        self.assertTrue(b'' in response.data) # There should be no message returned when users log out.
 
 if __name__ == '__main__':
     unittest.main()
