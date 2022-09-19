70fa73d8000c959e922f4f63f79a53240bb2b291
fangtingyu <83152253+fangtingyu@users.noreply.github.com>
2022-09-19 11:58:40 +0800

    added password column in user table

--- app/models.py
+++ app/models.py
@@ -11,6 +11,7 @@ class User(db.Model,UserMixin): #User table
 
   id = db.Column(db.Integer, primary_key=True) # Each user has only one unique id;
   email = db.Column(db.String(120), index=True, unique=True) 
+  password = db.Column(db.String(128))
   password_hash = db.Column(db.String(128))
 
 
