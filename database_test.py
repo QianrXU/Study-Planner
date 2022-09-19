7920dd43726446b466a687957633ca7be86686a1
fangtingyu <83152253+fangtingyu@users.noreply.github.com>
2022-09-19 11:46:19 +0800

    Schema testing draft

---
+++ database_test.py
@@ -0,0 +1,6 @@
+# The database test followed the 
+# https://www.softwaretestinghelp.com/database-testing-process/#1_Transactions
+
+# Database are tested to ensure (1) Data Mapping (2) ACID Properties Validation and (3) Data Integrity
+
+# Transaction Testing
